8963043c36d564bde6a95c7e52fd530ecaf6a8ca
Jack de Grave <jack.degrave@outlook.com>
2019-06-01 16:16:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
     data.each do  |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          delet_if == "strawberry"
+          delet_if "strawberry"
       end
     end
   end
